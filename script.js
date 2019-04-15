b49a9d815df0cc360b80dcc67af1489805e1239a
monikawi <44977764+monikawi@users.noreply.github.com>
2019-04-15 17:45:18 +0100

    upload

---
+++ script.js
@@ -0,0 +1,90 @@
+function promiseAll(promises) {
+  // Twój kod tu
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let pending = promises.length;
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(result => {
+        results[i] = result; 
+        pending--;
+        if (pending == 0) resolve(results);
+      }).catch(reject);
+    }
+    if (promises.length == 0) resolve(results);
+  });
+}
+
+function promiseRace(promises) {
+  // Twój kod tu
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      if (promises[i].then) {
+        promises[i].then(resolve, reject);
+      } else {
+        return resolve(promises[i]);
+      }
+    }
+  });
+}
+
+
+
+// Kod testowy.
+promiseAll([]).then(result => {
+  console.log('To powinien być []:', JSON.stringify(result));
+});
+
+promiseAll([futureSuccess(1), futureSuccess(2), futureSuccess(3)]).then(result => {
+  console.log('To powinien być [1, 2, 3]:', result);
+});
+
+promiseAll([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
+  .then(() => {
+    console.log('WAT?! Nie powinno nas tu być..');
+  })
+  .catch(error => {
+    if (error !== 'X') {
+      console.log('Coś poszło nie tak..:', error);
+    }
+    console.log('To powinien być X:', error);
+  });
+
+promiseRace([1, 2, 3]).then(result => {
+  console.log('This should be 1:', result);
+});
+
+const now = performance.now();
+promiseRace([delayedSuccess(1, 300), delayedSuccess(2, 200), delayedSuccess(3, 100)]).then(result => {
+  const after = performance.now();
+  const diff = after - now;
+  if (diff < 100) {
+    throw 'Za szybko!'
+  }
+  if (diff >= 200) {
+    throw 'Za wolno!'
+  }
+  console.log('To powinno być 3:', result);
+});
+
+promiseRace([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
+  .then(() => {
+    console.log('WAT?! Nie powinno nas tu być..');
+  })
+  .catch(error => {
+    if (error !== 'X') {
+      console.log('Coś poszło nie tak..:', error);
+    }
+    console.log('To powinien być X:', error);
+  });
+
+function futureSuccess(val) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+};
+
+function delayedSuccess(val, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(val), time);
+  });
+};
